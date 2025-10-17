f29270b6203d7a4ee54a9fad09c4c36cd601e500
nasdsadasd <nasdsadasd@kjaruga114-a11y>
2025-10-17 15:52:40 +0200

    utils

--- stacjaPogodowa/stacjaPogodowa/Program.cs
+++ stacjaPogodowa/stacjaPogodowa/Program.cs
@@ -8,13 +8,6 @@ Console.ReadKey();
 var weather2 = new Weather();
 var weather5 = new Weather();
 
-weather5 = ChangeWeather(weather2);
- Weather ChangeWeather(Weather weather)
-{
-    var objectToChange = new Weather();
-   // objectToChange.Id = weather.Id;
-    Console.WriteLine($"Weather measured by:{weather.MeasuredBy}");
-    objectToChange.MeasuredBy = "Jan Kowalski";
-   // weather.MeasurementDate = DateTime.Now;
-    return objectToChange;
-}
+var utils = new Utils();
+
+weather5 = utils.ChangeWeather(weather2);
